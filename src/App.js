8278f14132c71ef2f2975efd53f22058ea38a932
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-02 20:31:48 -0500

    Content component built

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 import Title from './Title';
+import POPOSList from './POPOSList';
 
 function App() {
   return (
     <div className='App'>
       <Title />
-      <img src={logo} alt="Logo"/>
+      <POPOSList />
     </div>
   );
 }
---
+++ src/POPOSList.js
@@ -0,0 +1,41 @@
+import React from "react"
+import POPOSSpace from "./POPOSSpace"
+
+function POPOSList() {
+    return (
+      <div>
+        <POPOSSpace
+        name="California Street"
+        address="50 California St."
+        image="50-california-st.jpg"
+        />
+        <POPOSSpace 
+        name="Pine Place"
+        address="100 Pine St."
+        image="100-pine.jpg"
+        />
+        <POPOSSpace 
+        name="California Steps"
+        address="101 California St."
+        image="101-california.jpg"
+        />
+        <POPOSSpace 
+        name="Sansome Roof Garden"
+        address="343 Sansome Ave."
+        image="343-sansome-roof-garden.jpg"
+        />
+       <POPOSSpace
+        name="Market Street Plaza"
+        address="525 Market St."
+        image="525-market-street-plaza.jpg"
+      />
+      <POPOSSpace
+        name="Citigroup Center"
+        address="1 Sansome St."
+        image="citigroup-center.jpg"
+      />
+      </div>
+    )
+  }
+  
+  export default POPOSList
