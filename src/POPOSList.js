2bbf3537435e0a2cf2a764e10a6edf3c37bf0c10
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-02 22:36:48 -0500

    Updated POPOSList to dynamically display the spaces with data from a JSON file

--- src/POPOSList.js
+++ src/POPOSList.js
@@ -1,40 +1,23 @@
 import React from "react"
 import POPOSSpace from "./POPOSSpace"
 import './POPOSList.css'
+import data from './sfpopos-data.json'
 
 function POPOSList() {
+  const spaces = data.map(( { title, address, hours, images } ) => {
+    return (
+      <POPOSSpace
+        key={title} 
+        name={title}
+        address={address}
+        hours={hours}
+        image={images[0]}
+      />
+    )
+  })
     return (
       <div className="POPOSList">
-        <POPOSSpace
-        name="California Street"
-        address="50 California St."
-        image="50-california-st.jpg"
-        />
-        <POPOSSpace 
-        name="Pine Place"
-        address="100 Pine St."
-        image="100-pine.jpg"
-        />
-        <POPOSSpace 
-        name="California Steps"
-        address="101 California St."
-        image="101-california.jpg"
-        />
-        <POPOSSpace 
-        name="Sansome Roof Garden"
-        address="343 Sansome Ave."
-        image="343-sansome-roof-garden.jpg"
-        />
-       <POPOSSpace
-        name="Market Street Plaza"
-        address="525 Market St."
-        image="525-market-street-plaza.jpg"
-      />
-      <POPOSSpace
-        name="Citigroup Center"
-        address="1 Sansome St."
-        image="citigroup-center.jpg"
-      />
+        { spaces }
       </div>
     )
   }
