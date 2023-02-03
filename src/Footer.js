ade701aca1594b09ddbd36172082c50c1409fa75
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-02 21:13:48 -0500

    Added a Footer component and completed Stretch challenges

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Footer.css'
+
+function Footer() {
+  const date = new Date().getFullYear();
+    return(
+      <div className="Footer">
+        <footer>
+          <ul>
+            <li><a href="https://sf.gov/location/san-francisco-city-hall">SF City Hall</a></li>
+            <li><a href="https://www.accuweather.com/en/us/san-francisco/94103/weather-forecast/347629">SF Weather</a></li>
+            <li><a href="https://www.bart.gov/">BART</a></li>
+          </ul>
+          Marcia McDonald-Scott cc {date}</footer>
+        </div>
+    );
+}
+
+export default Footer;
