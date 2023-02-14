a36dcb0cb927baae3c4a51cdc1492ebeeb871977
mcdott <111994803+mcdott@users.noreply.github.com>
2023-02-14 13:02:07 -0500

    Added search form to display spaces for which the title or address matches the search

--- src/components/POPOSList/POPOSList.js
+++ src/components/POPOSList/POPOSList.js
@@ -1,22 +1,63 @@
 import React from "react";
+import { useState } from "react";
+
 import POPOSSpace from "../POPOSSpace/POPOSSpace";
 import "./POPOSList.css";
 import data from "../../sfpopos-data.json";
 
 function POPOSList() {
-  const spaces = data.map(({ title, address, hours, images }, i) => {
-    return (
-      <POPOSSpace
-        id={i}
-        key={title}
-        name={title}
-        address={address}
-        hours={hours}
-        image={images[0]}
-      />
-    );
-  });
-  return <div className='POPOSList'>{spaces}</div>;
+  const [query, setQuery] = useState("");
+
+  const spaces = data
+    .filter((obj) => {
+      // true if query is in title
+      const inTitle = obj.title.toLowerCase().includes(query.toLowerCase());
+      // true if query is in address
+      const inAddress = obj.address.toLowerCase().includes(query.toLowerCase());
+      // return true if either is true
+      return inTitle || inAddress;
+    })
+    .map(({ title, address, hours, images }, i) => {
+      return (
+        <POPOSSpace
+          id={i}
+          key={title}
+          name={title}
+          address={address}
+          hours={hours}
+          image={images[0]}
+        />
+      );
+    });
+  return (
+    <div className='POPOSList'>
+      <form>
+        <input
+          value={query}
+          placeholder='search'
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+      {spaces}
+    </div>
+  );
 }
 
 export default POPOSList;
+
+// function POPOSList() {
+//   const spaces = data.map(({ title, address, hours, images }, i) => {
+//     return (
+//       <POPOSSpace
+//         id={i}
+//         key={title}
+//         name={title}
+//         address={address}
+//         hours={hours}
+//         image={images[0]}
+//       />
+//     );
+//   });
+//   return <div className='POPOSList'>{spaces}</div>;
+// }
